4b40948707d936683b6b13c667a1c844854013c3
Prajyot Pimpale <pimpaleprajyot@gmail.com>
2026-05-18 09:10:01 +0000

    basic practice added

---
+++ Practice/Basics/MyInfo.java
@@ -0,0 +1,10 @@
+public class MyInfo {
+    public static void main(String[] args) {
+        System.out.println("===== My Info =====");
+        System.out.println("Name  : Your Name");
+        System.out.println("Age   : 20");
+        System.out.println("City  : Mumbai");
+        System.out.printf("Java Version: %d%n", 17);
+        System.out.println("===================");
+    }
+}

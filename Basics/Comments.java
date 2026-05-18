b724c6deb5011e9ea5722ea61332293c017c0361
Prajyot Pimpale <pimpaleprajyot@gmail.com>
2026-05-18 09:03:28 +0000

    Comment file created.

---
+++ Basics/Comments.java
@@ -0,0 +1,19 @@
+public class Comments {
+    public static void main(String[] args) {
+
+        // This is a single-line comment
+
+        /*
+          This is a
+          multi-line comment
+        */
+
+        /**
+         * This is a Javadoc comment
+         * Used to generate documentation
+         * @author Alice
+         */
+
+        System.out.println("Comments don't affect output");
+    }
+}

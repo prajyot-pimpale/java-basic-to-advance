84d95a922dfad17ac2c68c8eecfd3ba7682071e3
Prajyot Pimpale <pimpaleprajyot@gmail.com>
2026-06-10 05:15:43 +0000

    MatrixMultiplication file added.

---
+++ Practice/MatrixMultiplication.java
@@ -0,0 +1,12 @@
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        int[][] a = {{1,2},{3,4}};
+        int[][] b = {{5,6},{7,8}};
+        int[][] c = new int[2][2];
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                for (int k = 0; k < 2; k++)
+                    c[i][j] += a[i][k] * b[k][j];
+        // c = [[19,22],[43,50]]
+    }
+}

d7f0517e8f80cbe9f394f5a2e4bce5e773797772
Prajyot Pimpale <pimpaleprajyot@gmail.com>
2026-06-11 05:22:45 +0000

    ReverseString class added.

---
+++ Practice/ReverseString.java
@@ -0,0 +1,6 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        System.out.println(new StringBuilder(str).reverse().toString());
+    }
+}
